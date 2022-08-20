f7cb2c370997003f6c94fbc33c256ef38aabcd2d
shubhibishnoi <72508847+shubhibishnoi@users.noreply.github.com>
2022-08-20 15:50:05 +0530

    Time: 70 ms (21.97%), Space: 14.3 MB (35.21%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& m, int k) {
-        int l = m[0][0];
-        int n = m.size();
-        int h = m[n-1][n-1];
+//         int l = m[0][0];
+//         int n = m.size();
+//         int h = m[n-1][n-1];
 
-    while(l<h){
-        int mid =l+ (h - l)/2;
-        int cnt =0;
-        for(int i=0;i<n;i++){
-            cnt += upper_bound(m[i].begin(),m[i].end(), mid) - m[i].begin();
-        }
+//     while(l<h){
+//         int mid =l+ (h - l)/2;
+//         int cnt =0;
+//         for(int i=0;i<n;i++){
+//             cnt += upper_bound(m[i].begin(),m[i].end(), mid) - m[i].begin();
+//         }
         
-        if(cnt<k)
-            l =mid+1;
+//         if(cnt<k)
+//             l =mid+1;
             
         
         
-        else h=mid;
+//         else h=mid;
        
 
-    }
+//     }
         
-        return l;
+//         return l;
+        
+
+    
+        priority_queue<int>pq; int n = m.size();
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++){
+                pq.push(m[i][j]);
+                if(pq.size() > k) pq.pop();
+            }
+        return pq.top();   
     }
 };
 
-//   int kthSmallest(vector<vector<int>>& matrix, int k) {
-        
-//         int n = matrix.size();
-//         int low = matrix[0][0];
-//         int high = matrix[n-1][n-1];
-//         while(low < high){
-            
-//             int mid = low + (high - low)/2;
-//             int count = 0;
-//             for(int i = 0; i < n; i++){
-//                 count += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
-                
-//             }
-            
-//             if(count < k){
-//                 low = mid + 1;
-//             }else{
-//                 high = mid;
-//             }
-            
-//         }
-        
-//         return low;
-//     }
-// };
