4519cbef949a0c1c71c15297401472b811c61237
shubhibishnoi <72508847+shubhibishnoi@users.noreply.github.com>
2022-08-22 17:16:13 +0530

    Time: 214 ms (17.01%), Space: 59.8 MB (12.46%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -4,10 +4,10 @@ public:
         int l=0;
         int h= arr.size()-1;
         if(h+1<3) return -1;
-        while(l<=h){
+        while(l<h){
             int m = l+(h-l)/2;
-            if(arr[m]> arr[m+1] && arr[m-1<arr[m]] ){
-               h = m-1;
+            if(arr[m]> arr[m+1]){
+               h = m;
                 
                 
             }
