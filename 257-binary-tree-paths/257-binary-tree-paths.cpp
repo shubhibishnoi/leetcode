aefe86e684dc278f86f0da0e2b722091436dc36b
shubhibishnoi <72508847+shubhibishnoi@users.noreply.github.com>
2022-08-24 18:27:39 +0530

    Time: 15 ms (7.49%), Space: 13.1 MB (48.66%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+  void dfs(vector<string>&v , string s , TreeNode*root ){
+        
+        
+         if(root==NULL){
+             return ;
+         }
+        
+        if(s.empty()){  //when empty insert root->val'
+            //The to_string() method accepts a single integer and converts the integer value or other data type value into a string.
+
+             s+=to_string(root->val);
+         }
+        else{
+            s+="->";
+            s+=to_string(root->val);
+        }
+        
+         if(root->left==NULL && root->right == NULL){
+             v.push_back(s);
+             s="";
+         }
+        
+         
+        
+ dfs(v,s,root->left);
+         dfs(v,s,root->right);
+        
+        
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+         vector<string>v;
+        
+          dfs(v,"",root);
+        
+        return v;
+    }
+
+        
+        
+   
+};
