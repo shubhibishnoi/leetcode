9c3c3488dd5cf72773effab86d84654459e33466
shubhibishnoi <72508847+shubhibishnoi@users.noreply.github.com>
2022-08-28 18:44:57 +0530

    Time: 3 ms (68.75%), Space: 8.2 MB (14.54%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -11,12 +11,8 @@
  */
 class Solution {
 public:
-    TreeNode* bstToGst(TreeNode* root) {
-        int acc_sum = 0;
-        sum_node(root,acc_sum);
-        return root;
-    }
     
+        
     void sum_node(TreeNode* node, int& acc_sum){
         if (node == NULL)  return;
         sum_node(node->right,acc_sum);
@@ -24,4 +20,10 @@ public:
         acc_sum = node->val;
         sum_node(node->left, acc_sum);
     }
+    TreeNode* bstToGst(TreeNode* root) {
+        int acc_sum = 0;
+        sum_node(root,acc_sum);
+        return root;
+    }
+
 };
