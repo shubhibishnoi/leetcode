861215b90ffdf29065a33d85f4aa3a69aa567811
shubhibishnoi <72508847+shubhibishnoi@users.noreply.github.com>
2022-09-22 22:25:48 +0530

    Time: 26 ms (45.88%), Space: 9.6 MB (90.16%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -5,10 +5,16 @@ public:
         for (int j = 0; j < s.size(); ++j) {
             if (s[j] == ' ') {
                 reverse(s.begin() + i, s.begin() + j);
-                i = j + 1;
+                i = j + 1;     
             }
         }
         reverse(s.begin() + i, s.end());
         return s;
+        
     }
 };
+
+
+
+
+
